e78fe23125e1912d814197e7afb7575ef1cd7510
Mai Campbell <jirallc@gmail.com>
2019-04-18 02:44:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-
+    delete object.key;
+    reuturn  object;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
